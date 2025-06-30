f3f951dac4afa395256b25f854e52ef61594da62
raulkm108 <raulkm.civil@gmail.com>
2025-06-30 10:32:06 -0300

    created get for the attributes
    Since the attributes are encapsulated, we need to create a function to call it

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ class Character:
         self.__name = name
         self.__hp = hp
         self.__lvl = lvl
+
+    def get_name(self):
+        return self.__name
+    
+    def get_hp(self):
+        return self.__hp
+    
+    def get_lvl(self):
+        return self.__lvl
